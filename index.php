6e6d4310ba8a4b389e194337ca97b2ce2d60d1fe
Laura <laura.hantz@hotmail.fr>
2019-12-13 14:10:14 +0100

    ok

--- index.php
+++ index.php
@@ -7,26 +7,26 @@ require('class/Triangle.php');
 $square1 = new Square(10); // instancie la classe // construit un carré de 10
 $square2 = new Square(24);
 
-echo 'Le carré a un périmètre de ', $square1->perimeter();
+echo 'Le carré 1 a un périmètre de ', $square1->perimeter();
 echo '<br>';
-echo 'Le carré a une surface de ', $square2->perimeter();
+echo 'Le carré 1 a une surface de ', $square2->perimeter();
 echo '<br>';
 
-echo 'Le carré a un périmètre de ', $square1->area();
+echo 'Le carré 2 a un périmètre de ', $square1->area();
 echo '<br>';
-echo 'Le carré a une surface de ', $square2->area();
+echo 'Le carré 2 a une surface de ', $square2->area();
 echo '<br>';
 
 $round1 = new Round(8);
-echo 'Le cercle a un périmètre de ', $round1->perimeter();
+echo 'Le cercle 1 a un périmètre de ', $round1->perimeter();
 echo '<br>';
-echo 'Le cercle a une surface de ', $round1->area();
+echo 'Le cercle 1 a une surface de ', $round1->area();
 echo '<br>';
 
 $triangle1 = new Triangle(9,12,15);
-echo 'Le triangle a un périmètre de ', $triangle1->perimeter();
+echo 'Le triangle 1 a un périmètre de ', $triangle1->perimeter();
 echo '<br>';
-echo 'Le triangle a une surface de ', $triangle1->area();
+echo 'Le triangle 1 a une surface de ', $triangle1->area();
 echo '<br>';
 
 
