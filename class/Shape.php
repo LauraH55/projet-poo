62e24168dbb03ee3b4a7d3badd748fbba3d21a72
Laura <laura.hantz@hotmail.fr>
2019-12-13 14:05:32 +0100

    Triangle ok

--- class/Shape.php
+++ class/Shape.php
@@ -3,4 +3,5 @@
 abstract class Shape // classe abstraite qu'on ne peux pas instancier
 {
   abstract public function perimeter();
+  abstract public function area();
 }
--- index.php
+++ index.php
@@ -11,19 +11,23 @@ echo 'Le carré a une surface de ', $square1->perimeter();
 echo '<br>';
 echo 'Le carré a une surface de ', $square2->perimeter();
 echo '<br>';
+
 echo 'Le carré a une aire de ', $square1->area();
 echo '<br>';
 echo 'Le carré a une aire de ', $square2->area();
 echo '<br>';
+
 $round1 = new Round(8);
 echo 'Le cercle a un périmètre de ', $round1->perimeter();
 echo '<br>';
 echo 'Le cercle a une aire de ', $round1->area();
 echo '<br>';
+
 $triangle1 = new Triangle(9,12,15);
 echo 'Le triangle a un périmètre de ', $triangle1->perimeter();
 echo '<br>';
 echo 'Le triangle a un aire de ', $triangle1->area();
+echo '<br>';
 
 
 
